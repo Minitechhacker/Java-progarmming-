c84103dcc7aa7ef39b66a06c3eaa0f77ba25216b
Mini_tech_hacker <57276723+Minitechhacker@users.noreply.github.com>
2020-01-13 14:25:15 +0530

    Add files via upload

---
+++ methodoverloading/src/methodoverloading/Method.java
@@ -0,0 +1,25 @@
+package methodoverloading;
+
+class over
+{
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int add(int a,int b,int c)
+	{
+		return a+b+c;
+	}
+}
+public class Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		over o=new over();
+		System.out.println(o.add(10, 15));
+		System.out.println(o.add(15, 20, 25));
+		
+
+	}
+
+}
---
+++ methodoverloading/src/methodoverloading/Static.java
@@ -0,0 +1,23 @@
+package methodoverloading;
+class st
+{
+	public static int a=0;
+	public void increment()
+	{
+		a++;
+		System.out.println(a);
+	}
+}
+public class Static {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     st s=new st();
+     st s1=new st();
+     st s3=new st();
+   s.increment();
+   s1.increment();
+   s3.increment();
+	}
+
+}
