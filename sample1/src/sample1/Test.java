e48a4379e47ca82d9f0416652e0a8505806dc15f
Mini_tech_hacker <57276723+Minitechhacker@users.noreply.github.com>
2020-01-13 14:24:52 +0530

    Add files via upload

---
+++ sample1/src/sample1/Test.java
@@ -0,0 +1,16 @@
+package sample1;
+
+public class Test {
+	
+
+	public void add()
+	{
+	 System.out.println("im test class");	
+	
+	}
+	public void display()
+	{
+		System.out.println("im test2 class");
+	}
+
+}
