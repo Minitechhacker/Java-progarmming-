4e078df4e7792a81508fe59c1f2055e51b48580e
Mini_tech_hacker <57276723+Minitechhacker@users.noreply.github.com>
2020-01-28 15:50:13 +0530

    Add files via upload

---
+++ package/src/Meena/Sri.java
@@ -0,0 +1,12 @@
+package Meena;
+
+public class Sri {
+
+	public void iphone_se_2()
+	{
+		System.out.println("It will be launched in March 2k2k ");
+	}
+	public void one_plus() {
+		System.out.println("I'm waiting for new updates");
+	}
+}
---
+++ package/src/Sri/Meena.java
@@ -0,0 +1,15 @@
+package Sri;
+
+import Meena.Sri;
+
+public class Meena {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Sri ph=new Sri();
+		ph.iphone_se_2();
+		ph.one_plus();
+	}
+
+}
